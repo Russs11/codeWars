b307088130f06ccd4871108674e59dcec7e181e1
Russs11 <russs.khalilov@gmail.com>
2022-11-11 16:04:44 +0300

    5 kyu_generateHashtag_11.11.2022

--- codeWars.js
+++ codeWars.js
@@ -166,7 +166,35 @@
 // I have removed the use of BigInteger and BigDecimal in java
 
 // Python: your solution need to work with huge numbers(about a milion digits), converting to int will not work.
-function sumStrings(a, b) {
-    const sum = BigInt(a) + BigInt(b)
-    return sum.toString()
+// function sumStrings(a, b) {
+//     const sum = BigInt(a) + BigInt(b)
+//     return sum.toString()
+// }
+
+// The marketing team is spending way too much time typing in hashtags.
+//     Let's help them with our own Hashtag Generator!
+
+// Here's the deal:
+
+// It must start with a hashtag(#).
+// All words must have their first letter capitalized.
+// If the final result is longer than 140 chars it must return false.
+// If the input or the result is an empty string it must return false.
+//     Examples
+// " Hello there thanks for trying my Kata"  => "#HelloThereThanksForTryingMyKata"
+// "    Hello     World   "                  => "#HelloWorld"
+// ""                                        => false
+function generateHashtag(str) {
+
+    str = str.replace(/\b\w/g, (c) => c.toUpperCase());
+    const splitted = str.split(" ");
+    const unshifted = splitted.unshift("#");
+    const res = splitted.join("");
+    if (res === "#") {
+        return false;
+    } else if (res.length > 140) {
+        return false;
+    } else {
+        return res
+    }
 }
