0a972f1cfa405b62ceb938fbf148375ccd2cb2bc
Russs11 <russs.khalilov@gmail.com>
2022-11-18 12:01:06 +0300

    5_kyu_odd_or_even_08.11.2022

--- codeWars.js
+++ codeWars.js
@@ -206,8 +206,19 @@
 // spinWords("Hey fellow warriors") => returns "Hey wollef sroirraw"
 // spinWords("This is a test") => returns "This is a test"
 // spinWords("This is another test")=> returns "This is rehtona test"
-function spinWords(words) {
-    return words.split(' ').map(function (word) {
-        return (word.length > 4) ? word.split('').reverse().join('') : word;
-    }).join(' ');
+// function spinWords(words) {
+//     return words.split(' ').map(function (word) {
+//         return (word.length > 4) ? word.split('').reverse().join('') : word;
+//     }).join(' ');
+// }
+// You will be given an array of numbers.You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+//     Examples
+// [7, 1]  => [1, 7]
+// [5, 8, 6, 3, 4]  => [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  => [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+function sortArray(array) {
+    const odds = array.filter((x) => x % 2).sort((a, b) => a - b);
+    return array.map((x) => x % 2 ? odds.shift() : x)
 }
