350d57ba51a8747c4f254ae8b85e5aac6f8965eb
Russs11 <russs.khalilov@gmail.com>
2022-11-21 14:25:15 +0300

    Simple multiplicatio_21.11.2022

--- codeWars.js
+++ codeWars.js
@@ -259,14 +259,18 @@
 
 // if bmi > 30 return "Obese"
 
-function bmi(weight, height) {
-    let bmi = weight / (height * height);
-    if (bmi <= 18.5)
-        return "Underweight";
-    if (bmi <= 25)
-        return "Normal";
-    if (bmi <= 30)
-        return "Overweight";
-    if (bmi > 30)
-        return "Obese";
+// function bmi(weight, height) {
+//     let bmi = weight / (height * height);
+//     if (bmi <= 18.5)
+//         return "Underweight";
+//     if (bmi <= 25)
+//         return "Normal";
+//     if (bmi <= 30)
+//         return "Overweight";
+//     if (bmi > 30)
+//         return "Obese";
+// }
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+function simpleMultiplication(number) {
+    return number % 2 === 0 ? number * 8 : number * 9;
 }
