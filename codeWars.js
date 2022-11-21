a5593ef0cb748fdd393365fc78bd542f8acc13c3
Russs11 <russs.khalilov@gmail.com>
2022-11-21 14:30:42 +0300

    angleOfTriangle_21.11.2022

--- codeWars.js
+++ codeWars.js
@@ -271,6 +271,10 @@
 //         return "Obese";
 // }
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
-function simpleMultiplication(number) {
-    return number % 2 === 0 ? number * 8 : number * 9;
+// function simpleMultiplication(number) {
+//     return number % 2 === 0 ? number * 8 : number * 9;
+// }
+// find third angle of triangle
+function otherAngle(a, b) {
+    return c = 180 - (a + b)
 }
