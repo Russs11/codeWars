a8eac2ecdb48be64ca30efe101d23be817cb7157
Russs11 <russs.khalilov@gmail.com>
2022-11-22 13:43:45 +0300

    find a needle_22.11.2022

--- codeWars.js
+++ codeWars.js
@@ -274,7 +274,27 @@
 // function simpleMultiplication(number) {
 //     return number % 2 === 0 ? number * 8 : number * 9;
 // }
-// find third angle of triangle
-function otherAngle(a, b) {
-    return c = 180 - (a + b)
+// // find third angle of triangle
+// function otherAngle(a, b) {
+//     return c = 180 - (a + b)
+// }
+
+// Can you find the needle in the haystack ?
+
+//     Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message(as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// Example(Input-- > Output)
+
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]-- > "found the needle at position 5"
+// Note: In COBOL, it should return "found the needle at position 6"
+function findNeedle(haystack) {
+    if (haystack.includes("needle")) {
+        return `found the needle at position ${haystack.indexOf("needle")}`;
+    }
 }
+
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]))
