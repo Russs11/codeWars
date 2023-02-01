2b6bce7f0cc09d479e077d2e6fbd5bbc8887e3b6
Russs11 <russs.khalilov@gmail.com>
2023-02-01 14:46:28 +0300

    _01.02.2023

--- codeWars.js
+++ codeWars.js
@@ -518,3 +518,12 @@
 // }
 
 // goals(10, 5, 2)
+// Given a non - empty array of integers, return the result of multiplying the values together in order.Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x) {
+return x.reduce((a, b) => a * b);
+}
+
+grow([4, 1, 1, 1, 4])
