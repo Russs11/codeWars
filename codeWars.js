264ee3c10469947b10102a44bbd85a19f0812a53
Russs <russs.khalilov@gmail.com>
2023-02-12 16:45:44 +0300

    12.02.2023

--- codeWars.js
+++ codeWars.js
@@ -583,13 +583,40 @@
 
 // Task:
 // Given a non - negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...".Input will always be valid, i.e.no negative integers.
-var countSheep = function (num) {
-    let res = []
-    for (let i = 0; i < num; i++){
-       res.push(`${i+1} sheep...`)
-    }
-    // return res.join('')
-    console.log(res.join(''));
-}
+// var countSheep = function (num) {
+//     let res = []
+//     for (let i = 0; i < num; i++){
+//        res.push(`${i+1} sheep...`)
+//     }
+//     // return res.join('')
+//     console.log(res.join(''));
+// }
 
-countSheep(3)
+// countSheep(3)
+
+// Write a function that takes an array of numbers and returns the sum of the numbers.The numbers can be negative or non - integer.If the array does not contain any numbers then you should return 0.
+
+// Examples
+// Input: [1, 5.2, 4, 0, -1]
+// Output: 9.2
+
+// Input: []
+// Output: 0
+
+// Input: [-2.398]
+// Output: -2.398
+
+// Assumptions
+// You can assume that you are only given numbers.
+// You cannot assume the size of the array.
+// You can assume that you do get an array and if the array is empty, return 0.
+// What We're Testing
+// We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
+// Advanced users may find this extremely easy and can easily write this in one line.
+
+function sum(numbers) {
+    "use strict";
+  return numbers.reduce((a,b) => a + b, 0)
+   console.log(res); 
+};
+sum([1, 5.2, 4, 0, -1])
