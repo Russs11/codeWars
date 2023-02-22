7d695a8b42ac430bd6bc0593cf54485f104b5348
Russs11 <russs.khalilov@gmail.com>
2023-02-22 16:31:35 +0300

    codWars_8_22.02.2023

--- codeWars.js
+++ codeWars.js
@@ -630,12 +630,32 @@
 // b = 4
 // --> [1, 2, 3, 4]
 
-function between(a, b) {
-    let res = []
-    for (let i = a; i <= b; i++){
-        res.push(i)
-    }
-    console.log(res);
-}
+// function between(a, b) {
+//     let res = []
+//     for (let i = a; i <= b; i++){
+//         res.push(i)
+//     }
+//     console.log(res);
+// }
 
-between(1, 4)
+// between(1, 4)
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+function fakeBin(x) {
+	let stringArr = x.split('')
+	let toNum = stringArr.map(Number);
+	for (let i = 0; i < toNum.length; i++) {
+		if (toNum[i] < 5) {
+			toNum[i] = 0
+		} else {
+			toNum[i] = 1
+		}
+	}
+
+	return toNum.join('')
+}
+function fakeBin(x) {
+	return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
