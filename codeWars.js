7fde48b3c892313a7c569d3c5ce1f4e61477face
Russs11 <russs.khalilov@gmail.com>
2023-03-02 17:15:55 +0300

    codWars_8_02.03.2023

--- codeWars.js
+++ codeWars.js
@@ -726,8 +726,22 @@
 // }
 
 // console.log(calculate(3, 5, multiply));
-function index(array, n) {
-	return !array[n]? -1 : array[n]**n
-}
-// index([1, 2, 3, 4], 2)
-console.log(index([1, 2, 3, 4], 5));
+// This kata is from check py.checkio.org
+
+// You are given an array with positive numbers and a non - negative number N.You should find the N - th power of the element in the array with the index N.If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+// Let's look at a few examples:
+
+// array = [1, 2, 3, 4] and N = 2, then the result is 3 ^ 2 == 9;
+// array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is - 1.
+// function index(array, n) {
+// 	return !array[n]? -1 : array[n]**n
+// }
+// // index([1, 2, 3, 4], 2)
+// console.log(index([1, 2, 3, 4], 5));
+// // Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid.Write a function to help Bob with this calculation.
+// class Kata {
+// 	static getVolumeOfCuboid(length, width, height) {
+// 		return length * width * height
+// 	}
+// }
