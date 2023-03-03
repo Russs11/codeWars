4b0b42697092b1fee2dcda6b07f1096b9f795afc
Russs11 <russs.khalilov@gmail.com>
2023-03-03 13:48:57 +0300

    codWars_7_03.03.2023

--- codeWars.js
+++ codeWars.js
@@ -745,3 +745,20 @@
 // 		return length * width * height
 // 	}
 // }
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument(also a string).
+
+// 	Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+function solution(str, ending) {
+	// TODO: complete
+	let arr = str.split('')
+	return arr.splice(arr.length - ending.length).join('').includes(ending);
+	// return str.includes(res)
+}
+
+
+// solution('abcde', 'cde')
+solution('abcde', 'cde')
