e85adb82a653966709c6935a429a57fd79cf9eca
Russs11 <russs.khalilov@gmail.com>
2023-03-03 15:58:25 +0300

    codWars_7_03.03.2023

--- codeWars.js
+++ codeWars.js
@@ -752,15 +752,41 @@
 
 // solution('abc', 'bc') // returns true
 // solution('abc', 'd') // returns false
-function solution(str, ending) {
-	// TODO: complete
-	let arr = str.split('')
-	return arr.splice(arr.length - ending.length).join('').includes(ending);
-	// return str.includes(res)
-}
-function solution2(str, ending) {
-	return str.endsWith(ending);
-}
+// function solution(str, ending) {
+// 	// TODO: complete
+// 	let arr = str.split('')
+// 	return arr.splice(arr.length - ending.length).join('').includes(ending);
+// 	// return str.includes(res)
+// }
+// function solution2(str, ending) {
+// 	return str.endsWith(ending);
+// }
 
+// // solution('abcde', 'cde')
 // solution('abcde', 'cde')
-solution('abcde', 'cde')
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers(Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example(just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+function gimme(triplet) {
+	let min = Math.min.apply(null, triplet)
+	let max = Math.max.apply(null, triplet)
+	for (let i = 0; i < triplet.length; i++){
+		if (triplet[i] < max && triplet[i] > min) {
+			return triplet.indexOf(triplet[i])
+		}
+	}
+	}
+
+
+console.log(gimme([5.9, 10.4, 14.2]));
+
