a18b4734c44453960c33b5d37ad3dd4f8ae0470e
Russs11 <russs.khalilov@gmail.com>
2023-03-09 17:41:58 +0300

    codWars_4_09.03.2023

--- codeWars.js
+++ codeWars.js
@@ -831,7 +831,19 @@
     
 // // SmallestInteger.findSmallestInt([78, 56, 232, 12, 8])
 // console.log( SmallestInteger.findSmallestInt([78, 56, 232, 12, 8]));
+// The marketing team is spending way too much time typing in hashtags.
+// 	Let's help them with our own Hashtag Generator!
 
+// Here's the deal:
+
+// It must start with a hashtag(#).
+// All words must have their first letter capitalized.
+// If the final result is longer than 140 chars it must return false.
+// If the input or the result is an empty string it must return false.
+// 	Examples
+// " Hello there thanks for trying my Kata"  => "#HelloThereThanksForTryingMyKata"
+// "    Hello     World   "                  => "#HelloWorld"
+// ""                                        => false
 function generateHashtag(str) {
 	let arr = str.split(' ')
 	// str.replace(str[0], )
