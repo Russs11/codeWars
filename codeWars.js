df4a27bac699b0a4620805c5aec171c6736c3c1e
Russs11 <russs.khalilov@gmail.com>
2023-03-13 17:40:20 +0300

    codeWars_8_13.03.2023

--- codeWars.js
+++ codeWars.js
@@ -1017,3 +1017,21 @@
 // }
 
 // declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew")
+// DESCRIPTION:
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// 	Examples(Input -> Output)
+// 	* [1, 2, 3, 4] -> [4, 3, 2, 1]
+// 	* [9, 2, 0, 7] -> [7, 0, 2, 9]
+
+function reverseList(list) {
+	let res = []
+	for (let i = list.length -1; i >= 0; i--){
+	res.push(list[i])
+	}
+	console.log(res);
+}
+function reverseList(list) {
+	return list.reverse();
+}
+reverseList([1, 2, 3, 4])
