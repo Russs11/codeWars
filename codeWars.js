9f40a9535140233dc9dfc9c892725094630a08b8
Russs <russs.khalilov@gmail.com>
2023-03-19 13:12:17 +0300

    _codeWars_8_19.03.2023

--- codeWars.js
+++ codeWars.js
@@ -1097,23 +1097,38 @@
 // abcdefuvwxyz("      12)  concatenation of strarr[4] and strarr[5]
 
 // Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
-// The first that came is "folingtrashy" so 
+// The first that came is "folingtrashy" so
 // longest_consec(strarr, 2) should return "folingtrashy".
 
 // In the same way:
 // longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2)-- > "abigailtheta"
 
 
-function longestConsec(strarr, k) {
-	let longest = "";
-	for (let i = 0; k > 0 && i <= strarr.length - k; i++) {
-		let tempStr = strarr.slice(i, i + k).join("");
-		if (tempStr.length > longest.length) {
-			longest = tempStr;
-		}
-	}
-	return longest;
+// function longestConsec(strarr, k) {
+// 	let longest = "";
+// 	for (let i = 0; k > 0 && i <= strarr.length - k; i++) {
+// 		let tempStr = strarr.slice(i, i + k).join("");
+// 		if (tempStr.length > longest.length) {
+// 			longest = tempStr;
+// 		}
+// 	}
+// 	return longest;
+// }
+
+// // longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)
+// console.log(longestConsec(["itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2));
+
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each.If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2) {
+    // moment of truth
+    if (flower1 % 2 !== 0 && flower2 % 2 === 0 || flower1 % 2 === 0 && flower2 % 2 !== 0) {
+        return true
+    } else {
+        return false
+    }
 }
 
-// longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)
-console.log(longestConsec(["itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2));
+lovefunc(1, 4)
