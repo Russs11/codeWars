bedbfb6cb90cbbe5da3a813223e5e73863a3ed1c
Russs11 <russs.khalilov@gmail.com>
2023-03-22 17:23:21 +0300

    codeWars_8_22.03.2023

--- codeWars.js
+++ codeWars.js
@@ -1192,16 +1192,92 @@
 
 // move(3, 6)
 // console.log('move(3, 6): ', move(3, 6));
-// 
+//
 
-function Ship(draft, crew) {
-	this.draft = draft;
-	this.crew = crew;
+// function Ship(draft, crew) {
+// 	this.draft = draft;
+// 	this.crew = crew;
+// }
+
+// Ship.prototype.isWorthIt = function () {
+// 	return 20 < this.draft - this.crew * 1.5;
+// };
+// const titanic = new Ship(15, 10)
+
+// console.log(titanic.isWorthIt());
+
+// Write a class Block that creates a block(Duh..)
+
+// Requirements
+// The constructor should take an array as an argument, this will contain 3 integers of the form[width, length, height] from which the Block should be created.
+
+// Define these methods:
+
+// `getWidth()` return the width of the`Block`
+
+// `getLength()` return the length of the`Block`
+
+// `getHeight()` return the height of the`Block`
+
+// `getVolume()` return the volume of the`Block`
+
+// `getSurfaceArea()` return the surface area of the`Block`
+// Examples
+// let b = new Block([2, 4, 6]) -> creates a `Block` object with a width of `2` a length of `4` and a height of`6`
+// b.getWidth() // -> 2
+
+// b.getLength() // -> 4
+
+// b.getHeight() // -> 6
+
+// b.getVolume() // -> 48
+
+// b.getSurfaceArea() // -> 88
+class Block {
+
+	constructor(data) {
+		// ...
+		this.width = data[0]
+		this.length = data[1]
+		this.height = data[2]
+
+	}
+	getWidth() {
+	return this.width
+	}
+	getLength() {
+		return this.length
+	}
+	getHeight() {
+		return this.height
+	}
+	getVolume() {
+		return this.width * this.height * this.length
+	}
+	getSurfaceArea() {
+		return 2 * ((this.width * this.length) + (this.length * this.height) + (this.width * this.height))
+	}
+	// ...
+
 }
 
-Ship.prototype.isWorthIt = function () {
-	return 20 < this.draft - this.crew * 1.5;
-};
-const titanic = new Ship(15, 10)
+class Block {
+	constructor(data) {
+		[this.w, this.l, this.h] = data;
+	}
+	getWidth() { return this.w }
+	getLength() { return this.l }
+	getHeight() { return this.h }
+	getVolume() {
+		let { l, w, h } = this;
+		return w * l * h;
+	}
+	getSurfaceArea() {
+		let { l, w, h } = this;
+		return 2 * (l * w + l * h + w * h);
+	}
+}
 
-console.log(titanic.isWorthIt());
+let b = new Block([2, 4, 6])
+
+console.log(b.getSurfaceArea());
