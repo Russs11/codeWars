075cf9130f62a900e59ef46fde57f41274171ffa
Russs11 <russs.khalilov@gmail.com>
2023-03-23 16:36:29 +0300

    codeWars_6_23.03.2023

--- codeWars.js
+++ codeWars.js
@@ -1282,17 +1282,44 @@ class Block {
 
 // console.log(b.getSurfaceArea());
 
-class Class {
+// class Class {
 
-	static getNumber() {
-	this.counter = 0
-		if (this.counter === 1) {
-			return 1; 
-		} else {
-			return this.counter * 2
-		}
+// 	static getNumber() {
+// 	this.counter = 0
+// 		if (this.counter === 1) {
+// 			return 1;
+// 		} else {
+// 			return this.counter * 2
+// 		}
+// 	}
+// }
+
+// // Class.getNumber()
+// console.log('Class.getNumber(): ', Class.getNumber());
+// You are developing an image hosting website.
+
+// You have to create a function for generating random and unique image filenames.
+
+// Create a function for generating a random 6 character string which will be used to access the photo URL.
+
+// To make sure the name is not already in use, you are given access to an PhotoManager object.
+
+// You can call it like so to make sure the name is unique
+
+// // at this point, the website has only one photo, hosted on the 'ABCDEF' url
+// photoManager.nameExists('ABCDEF'); // returns true
+// photoManager.nameExists('BBCDEF'); // returns false
+// Note: We consider two names with same letters but different cases to be unique.
+function generateName() {
+	const letters = 'ABCDEFGGIJKLMNOPQRSTUVWXYZ'
+	let res = ''
+	// make sure to check the name is unique via the photoManager object
+	for (let i = 0; i < 6; i++){
+		 res += letters[Math.floor(Math.random() * letters.length)]
 	}
+	
+	return res
 }
 
-// Class.getNumber()
-console.log('Class.getNumber(): ', Class.getNumber());
+// generateName()
+console.log('generateName(): ', generateName());
