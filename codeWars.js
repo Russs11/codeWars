73391248b518b499d41e6fbca0be427c25c1aed7
Russs <russs.khalilov@gmail.com>
2023-03-29 18:08:51 +0300

    _codeWars_7_arrays_map_29.03.2023

--- codeWars.js
+++ codeWars.js
@@ -1494,3 +1494,26 @@ function longest(s1, s2) {
 // function solution(nums) {
 //     return nums !== null ? nums.sort(function (a, b) { return a - b }) : [];
 // }
+// DESCRIPTION:
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string.Notice the colon and space in between.
+
+//     Examples: (Input-- > Output)
+
+//     []-- > []
+//     ["a", "b", "c"]-- > ["1: a", "2: b", "3: c"]
+const number = function (array) {
+    let res = []
+    for (let i = 0; i < array.length; i++) {
+        res.push((i + 1 + ': ') + array[i])
+    }
+    return res;
+}
+let number2 = function (array) {
+    return array.map(function (line, index) {
+        return (index + 1) + ": " + line;
+    });
+}
