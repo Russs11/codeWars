28c63d73728c0a282a8599a92fc24853079887a3
Russs <russs.khalilov@gmail.com>
2023-03-30 21:55:27 +0300

    _codeWars_8_arrays_map_30.03.2023

--- codeWars.js
+++ codeWars.js
@@ -1518,10 +1518,27 @@ function longest(s1, s2) {
 //     });
 // }
 
-function removeSmallest(numbers) {
-return numbers.filter((_,index) => index != numbers.indexOf(Math.min(... numbers)))
+// function removeSmallest(numbers) {
+// return numbers.filter((_,index) => index != numbers.indexOf(Math.min(... numbers)))
     
+// }
+
+// // removeSmallest([1, 2, 3, 4, 5])
+// console.log(removeSmallest([1, 2, 3, 4, 5]));
+
+// DESCRIPTION:
+// Write a function to convert a name into initials.This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+function abbrevName(name) {
+   return name.split(' ').map((el, el2) => el[0] ).join('.').toUpperCase()  
 }
 
-// removeSmallest([1, 2, 3, 4, 5])
-console.log(removeSmallest([1, 2, 3, 4, 5]));
+// abbrevName("Sam Harris")
+console.log(abbrevName("sam harris"));
