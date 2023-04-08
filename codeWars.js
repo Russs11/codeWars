f2b42e21d4421da65b8ac95a846f366769cd6894
Russs <russs.khalilov@gmail.com>
2023-04-08 22:51:24 +0300

    codeWars_8_array._08.04.2023

--- codeWars.js
+++ codeWars.js
@@ -1721,29 +1721,41 @@
 
 
 
-const userInfo = {
-	balance: 0,
-	operations: 0,
-	increase(sum) {
-		this.balance += sum;
-		this.operations ++
-	}
+// const userInfo = {
+// 	balance: 0,
+// 	operations: 0,
+// 	increase(sum) {
+// 		this.balance += sum;
+// 		this.operations ++
+// 	}
+// }
+
+// function user() {
+// 	const userObj = { ...userInfo }
+// 	return function () {
+// 		return userObj
+// 	}
+// }
+
+
+// const user1 = user()
+// user1().increase(100)
+// user1().increase(100)
+// console.log(user1());
+
+// const user2 = user()
+// user2().increase(100)
+
+// console.log(user2());
+
+// DESCRIPTION:
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(x) {
+    return x.reduce((acc, el) => {
+        return Number(acc) + Number(el)
+    }, 0)
 }
 
-function user() {
-	const userObj = { ...userInfo }
-	return function () {
-		return userObj
-	}
-}
-
-
-const user1 = user()
-user1().increase(100)
-user1().increase(100)
-console.log(user1());
-
-const user2 = user()
-user2().increase(100)
-
-console.log(user2());
