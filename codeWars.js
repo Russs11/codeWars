0a5f66189e91c6348487a7c4583eaf579c75676a
Russs11 <russs.khalilov@gmail.com>
2023-04-27 09:37:10 +0300

    codeWars_8_27.04.2023

--- codeWars.js
+++ codeWars.js
@@ -1753,9 +1753,21 @@
 
 // Return your answer as a number.
 
-function sumMix(x) {
-    return x.reduce((acc, el) => {
-        return Number(acc) + Number(el)
-    }, 0)
-}
+// function sumMix(x) {
+//     return x.reduce((acc, el) => {
+//         return Number(acc) + Number(el)
+//     }, 0)
+// }
+// Given a set of numbers, return the additive inverse of each.Each positive becomes negatives, and the negatives become positives.
+
+// 	invert([1, 2, 3, 4, 5]) == [-1, -2, -3, -4, -5]
+// invert([1, -2, 3, -4, 5]) == [-1, 2, -3, 4, -5]
+// invert([]) == []
+// You can assume that all values are integers.Do not mutate the input array / list.
+
+// function invert(array) {
+// 	return array.map((item) => item < 0 ? Math.abs(item) : -Math.abs(item))
+// }
+
+// const invert = array => array.map(num => -num);
 
