4c0e0913d5196ee04cdd71d407b6fc87b2f58f80
Russs <russs.khalilov@gmail.com>
2023-05-03 21:34:05 +0300

    _codeWars_8_arrays_sum of positive_03.05.2023

--- codeWars.js
+++ codeWars.js
@@ -1709,7 +1709,7 @@
 //         getName: function () {
 //             return this.name
 //         }
-        
+
 //     },
 //     getName: function () {
 //         return this.name
@@ -1780,8 +1780,8 @@
 // uniqueInOrder([1, 2, 2, 3, 3]) == [1, 2, 3]
 // var uniqueInOrder = function (iterable) {
 //     let res = []
-  
-   
+
+
 //     for (let i = 0; i < iterable.length; i++) {
 //         if (iterable[i] !== iterable[i-1]){
 //             res.push(iterable[i])
@@ -1791,3 +1791,20 @@
 // }
 
 // uniqueInOrder('AAAABBBCCDAABBB');
+// DESCRIPTION:
+// You get an array of numbers, return the sum of all of the positives ones.
+
+//     Example[1, -4, 7, 12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+// function positiveSum(arr) {
+//     return arr.reduce((a, b) => {
+//         if (b > 0) {
+//             return a + b
+//         }
+//         return a
+//     }, 0)
+// }
+
+// // positiveSum([-1, 4, -7, 5, 3])
+// console.log( positiveSum([-1, 4, -7, 5, 3]));
