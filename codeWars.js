54b1fba2872eaf84ff430352991baf35d06735de
Russs <russs.khalilov@gmail.com>
2023-05-06 21:43:06 +0300

    h_06.05.2023

--- codeWars.js
+++ codeWars.js
@@ -1808,3 +1808,10 @@
 
 // // positiveSum([-1, 4, -7, 5, 3])
 // console.log( positiveSum([-1, 4, -7, 5, 3]));
+// You will be given an array and a limit value.You must check that all values in the array are below or equal to the limit value.If they are, return true.Else, return false.
+
+// You can assume all values in the array are numbers.
+// function smallEnough(a, limit) {
+//     return a.map(item => { item <= limit ? true : false }).includes(false)? false : true
+// }
+// console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120));
