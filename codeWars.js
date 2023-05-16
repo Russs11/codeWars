0f1012eef809b92b9afd5cf84048eae191b1aee4
Russs11 <russs.khalilov@gmail.com>
2023-05-16 17:03:43 +0300

    w_16.05.2023

--- codeWars.js
+++ codeWars.js
@@ -1847,3 +1847,15 @@
 //     //your code here
 //     return string.split(' ').reverse().join(' ')
 // }
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+// don't worry about uppercase vowels
+// y is not considered a vowel for this kata
+// function shortcut(string) {
+// 	return string.split(/[aeiou]/g).join('')
+// }
