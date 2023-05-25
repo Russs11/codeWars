2e2ece618a04b572ec911f2274cf809c724bfd74
Russs11 <russs.khalilov@gmail.com>
2023-05-25 11:17:22 +0300

    w_25.05.2023

--- codeWars.js
+++ codeWars.js
@@ -1985,3 +1985,7 @@
 // }
 // // accum("ZpglnRxqenU")
 // console.log('accum("ZpglnRxqenU"): ', accum("ZpglnRxqenU"));
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health.Health can't be less than 0.
+// function combat(health, damage) {
+//     return health < damage ? 0 : health - damage
+// }
