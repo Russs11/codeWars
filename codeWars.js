ae6f1bc9945bd8309a88bf48cab86672de1e2b09
Russs11 <russs.khalilov@gmail.com>
2023-05-26 16:32:56 +0300

    w_26.05.2023

--- codeWars.js
+++ codeWars.js
@@ -2034,6 +2034,43 @@
 
 
 
-function checkForFactor(base, factor) {
-	return base % factor === 0 ? true : false
+// function checkForFactor(base, factor) {
+// 	return base % factor === 0 ? true : false
+// }
+
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// 	if x > y: 3 points(win)
+// if x < y: 0 points(loss)
+// if x = y: 1 point(tie)
+// We need to write a function that takes this collection and returns the number of points our team(x) got in the championship by the rules given above.
+
+// 	Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+	let counter = 0
+	for (const nums of games) {
+		if (+nums[0] > +nums[2]) {
+			counter += 3
+		} else if (+nums[0] === +nums[2]) {
+			counter += 1
+		}else if (+nums[0] < +nums[2]) {
+			counter += 0
+		}
+		
+	}
+	return counter
+	// return 0
 }
+
+points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"])
