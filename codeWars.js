76df114d7dbc408120dbe039b59c40100e9b1047
Russs11 <russs.khalilov@gmail.com>
2023-05-26 11:18:32 +0300

    w_26.05.2023

--- codeWars.js
+++ codeWars.js
@@ -1989,23 +1989,29 @@
 // function combat(health, damage) {
 //     return health < damage ? 0 : health - damage
 // }
+// Your task is to sort a given string.Each word in the string will contain a single number.This number is the position the word should have in the result.
 
-function order(words){
-    let counter = 0
-    let result = []
-    let arr = words.split(' ')
-    console.log(arr);
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j< arr.length; j++){
-            counter ++
-            console.log( arr[i].includes(arr[j]));
-            if(arr[i].includes(arr[j])){
-                
-            }
-        }
-        // arr[i] - arr[i]
-        console.log(result);
-    }
+// 	Note: Numbers can be from 1 to 9. So 1 will be the first word(not 0).
+
+// If the input string is empty, return an empty string.The words in the input String will only contain valid consecutive numbers.
+
+// 	Examples
+// "is2 Thi1s T4est 3a"  -- > "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -- > "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -- > ""
+function order(words) {
+	let arr = words.split(' ')
+	let r = []
+
+	arr.forEach(word => {
+		let x = word.split('')
+		let num = x.find(el => parseInt(el))
+		r.push([word, parseInt(num)])
+	})
+
+	r.sort((a, b) => a[1] - b[1]).map(x => x.splice(1, 1))
+	return r.join(' ')
 }
 
-order("is2 Thi1s T4est 3a")
+// order("is2 Thi1s T4est 3a")
+console.log('order("is2 Thi1s T4est 3a"): ', order("is2 Thi1s T4est 3a"));
