146a0c65b6b28efa5fb36d79fb8ed6969590a3f9
Russs <russs.khalilov@gmail.com>
2023-06-05 21:54:43 +0300

    h_05.06.2023

--- codeWars.js
+++ codeWars.js
@@ -2067,7 +2067,7 @@
 // 		}else if (+nums[0] < +nums[2]) {
 // 			counter += 0
 // 		}
-		
+
 // 	}
 // 	return counter
 // 	// return 0
@@ -2084,7 +2084,7 @@
 // * "1234!_ "     -> "11223344!!__  "
 
 // function doubleChar(str) {
-  
+
 //   return str.split('').map(item => {
 //     return item + item
 //   })
@@ -2146,3 +2146,55 @@
 
 // // countBy(2,5);
 // console.log(countBy(2,5));
+// DESCRIPTION:
+// In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+// make as few changes as possible.
+// if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+// For example:
+
+// solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+// solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+// solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+
+function solve(s) {
+    let arr = s.split('')
+    let upperCase = 0
+    let lowerCase = 0
+    let res = []
+    for (const item of arr) {
+        if (item == item.toUpperCase()) {
+            upperCase++
+        }
+        else {
+            lowerCase++
+        }
+        res.push(item)
+
+    }
+    if (upperCase > lowerCase) {
+        return res.join('').toUpperCase()
+    }
+    if (upperCase === lowerCase) {
+        return res.join('').toLowerCase()
+    }
+    else {
+        return res.join('').toLowerCase()
+    }
+}
+function solve(s) {
+    let lowerC = 0;
+    let upperC = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == s[i].toUpperCase()) {
+            upperC++;
+        }
+        else {
+            lowerC++;
+        }
+    }
+    return lowerC >= upperC ? s.toLowerCase() : s.toUpperCase()
+}
+
+console.log(solve("Code"));
