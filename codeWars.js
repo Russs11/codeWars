558485e3d0422ec5e91701026789206d13e3beb2
Russs <russs.khalilov@gmail.com>
2023-06-08 21:12:24 +0300

    h_08.06.2023

--- codeWars.js
+++ codeWars.js
@@ -2221,3 +2221,17 @@
 //       return "You need to give your plant " + (40 - mm) + "mm of water"
 //     return "Your plant has had more than enough water for today!" 
 //   }
+// DESCRIPTION:
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+// For example, when the input is green, output should be yellow.
+
+
+// function updateLight(current) {
+//     return current === "green" ? "yellow" : current === "yellow" ? "red" : "green"
+// }
+
+
+// console.log(updateLight("green"));
