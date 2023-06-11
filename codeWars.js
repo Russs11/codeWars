c01d186336056e6f3c3fba87f05572b2c947cd06
Russs <russs.khalilov@gmail.com>
2023-06-11 21:53:15 +0300

    h_11.06.2023

--- codeWars.js
+++ codeWars.js
@@ -2229,9 +2229,23 @@
 // For example, when the input is green, output should be yellow.
 
 
-function updateLight(current) {
-    return current === "green" ? "yellow" : current === "yellow" ? "red" : "green"
-}
+// function updateLight(current) {
+//     return current === "green" ? "yellow" : current === "yellow" ? "red" : "green"
+// }
 
 
-console.log(updateLight("green"));
+// console.log(updateLight("green"));
+
+
+// DESCRIPTION:
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+
+function paperwork(n, m) {
+    return n > 0 && m > 0 ? n * m : 0
+  }
