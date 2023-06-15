3cdb926a6a33a9e08d465ccab147f0d55362223e
Russs11 <russs.khalilov@gmail.com>
2023-06-15 12:36:03 +0300

    w_15.06.2023

--- codeWars.js
+++ codeWars.js
@@ -2219,7 +2219,7 @@
 // function rainAmount(mm){
 //     if (mm < 40)
 //       return "You need to give your plant " + (40 - mm) + "mm of water"
-//     return "Your plant has had more than enough water for today!" 
+//     return "Your plant has had more than enough water for today!"
 //   }
 // DESCRIPTION:
 // You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
@@ -2246,6 +2246,25 @@
 // n= 5, m=5: 25
 // n=-5, m=5:  0
 
-function paperwork(n, m) {
-    return n > 0 && m > 0 ? n * m : 0
-  }
+// function paperwork(n, m) {
+//     return n > 0 && m > 0 ? n * m : 0
+//   }
+// DESCRIPTION:
+// What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array ?
+
+// 	Example(Input-- > Output)
+
+// "apple ban" -- > ["apple 5", "ban 3"]
+// "you will win" -- > ["you 3", "will 4", "win 3"]
+// Your task is to write a function that takes a String and returns an Array / list with the length of each word added to each element.
+
+// 	Note: String will have at least one element; words will always be separated by a space.
+
+function addLength(str) {
+	return str.split(' ').map(item => {
+		return `${item} ${item.length}`
+	})
+	.join(' ')
+}
+// addLength('apple ban')
+console.log(addLength('apple ban'));
