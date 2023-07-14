f8ffbe2fdaed8062bc014449b0d6948baf5bca07
Russs <russs.khalilov@gmail.com>
2023-07-14 22:10:30 +0300

    h_14.07.2023

--- codeWars.js
+++ codeWars.js
@@ -2352,9 +2352,17 @@
 
 // You should not remove or add elements from/to the array.
 
-function twoSort(s) {
-  const sorted = s.sort(); // the default sort function works
-    return sorted[0] // take first element
-      .split("") // split string into chars
-      .join("***"); // join chars with ***
-  }
+// function twoSort(s) {
+//   const sorted = s.sort(); // the default sort function works
+//     return sorted[0] // take first element
+//       .split("") // split string into chars
+//       .join("***"); // join chars with ***
+//   }
+
+  // DESCRIPTION:
+  // Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+  
+  // If you need help, here's a reference:
+  function take(arr, n) {
+  return arr.slice(0, n)
+}
