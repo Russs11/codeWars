d0ebafdf49a2946b0ab8336a8fa966077d0685e2
Russs <russs.khalilov@gmail.com>
2023-07-21 15:45:55 +0300

    21.07.2023

--- codeWars.ts
+++ codeWars.ts
@@ -6,9 +6,34 @@
 // n = 1  ==> [1, 2]     # [2^0, 2^1]
 // n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 
-export function powersOfTwo(n:number):number[]{
-    let arr:number[] = [];
-  for(let i=0; i<=n; i++)
-    arr[i] = 2**i;
-  return arr;
-  }
+// export function powersOfTwo(n:number):number[]{
+//     let arr:number[] = [];
+//   for(let i=0; i<=n; i++)
+//     arr[i] = 2**i;
+//   return arr;
+//   }
+
+
+let info: {
+    officeId: number;
+    isOpened: boolean;
+    contacts:{
+        phone:string;
+		email: string;
+        address: {
+            city: string;
+        }
+    }
+} = {
+	"officeId": 45,
+	"isOpened": false,
+	"contacts": {
+		"phone": "+79100000000",
+		"email": "my@email.ru",
+		"address": {
+			"city": "Москва"
+		}
+	}
+}
+
+console.log(info);
