628218c4982fd86450de15a7e9676448a70142f0
Russs <russs.khalilov@gmail.com>
2023-10-24 22:26:21 +0300

    h_24.10.2023

--- javascript/codeWars.js
+++ javascript/codeWars.js
@@ -2402,13 +2402,35 @@
 // // Your task is to debug the code before your plants die!
 
 
-function rainAmount(mm){
-    if (mm < 40) {
-         return `You need to give your plant ${40 - mm}mm of water`
-    }
-   else if(mm > 40){
-         return "Your plant has had more than enough water for today!"
-    }else{
-     return "Your plant has had more than enough water for today!"
-    };
+// function rainAmount(mm){
+//     if (mm < 40) {
+//          return `You need to give your plant ${40 - mm}mm of water`
+//     }
+//    else if(mm > 40){
+//          return "Your plant has had more than enough water for today!"
+//     }else{
+//      return "Your plant has had more than enough water for today!"
+//     };
+// }
+
+// DESCRIPTION:
+// Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array or list ( depending on language ).
+
+// Examples
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+function countBy(x, n) {
+  let z = [];
+  for(let i = 1; i < n + 1; i++){
+  z.push(i * x)
+  }
+  return z
 }
+
+// countBy(2,5);
+console.log(countBy(2,5));
