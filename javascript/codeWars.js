0090199b1f5b40c124d30a8437dd3c2d5cae5eb8
Russs <russs.khalilov@gmail.com>
2023-10-25 21:33:45 +0300

    h_25.10.2023

--- javascript/codeWars.js
+++ javascript/codeWars.js
@@ -2424,13 +2424,30 @@
 // countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 // countBy(2,5) === [2,4,6,8,10]
 
-function countBy(x, n) {
-  let z = [];
-  for(let i = 1; i < n + 1; i++){
-  z.push(i * x)
-  }
-  return z
-}
+// function countBy(x, n) {
+//   let z = [];
+//   for(let i = 1; i < n + 1; i++){
+//   z.push(i * x)
+//   }
+//   return z
+// }
 
-// countBy(2,5);
-console.log(countBy(2,5));
+// // countBy(2,5);
+// console.log(countBy(2,5));
+
+// // DESCRIPTION:
+// // You have an award-winning garden and every day the plants need exactly 40mm of water. You created a great piece of JavaScript to calculate the amount of water your plants will need when you have taken into consideration the amount of rain water that is forecast for the day. Your jealous neighbour hacked your computer and filled your code with bugs.
+
+// // Your task is to debug the code before your plants die!
+
+
+function rainAmount(mm){
+    if (mm < 40) {
+         return `You need to give your plant ${40 - mm}mm of water`
+    }
+   else if(mm > 40){
+         return "Your plant has had more than enough water for today!"
+    }else{
+     return "Your plant has had more than enough water for today!"
+    };
+}
