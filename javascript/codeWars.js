c2532b83b4fa6911e6a174ffdeaa2eb44d885ab8
Russs <russs.khalilov@gmail.com>
2023-12-21 12:44:48 +0300

    h_21.12.2023

--- javascript/codeWars.js
+++ javascript/codeWars.js
@@ -2579,25 +2579,33 @@
 
 // console.log(vowelIndices('YoMama'));
 
-orderedCount = (text) => {
-  let x = text.split("")
-  let dic = {}
-  let res = []
-  let finalRes = []
+// orderedCount = (text) => {
+//   let x = text.split("")
+//   let dic = {}
+//   let res = []
+//   let finalRes = []
 
-  x.forEach((letter, idx) => {
-      !dic[letter] ? dic[letter] = [1, idx] : dic[letter][0] += 1
-  })
-  for (letter in dic){
-      res.push([letter, dic[letter]])
+//   x.forEach((letter, idx) => {
+//       !dic[letter] ? dic[letter] = [1, idx] : dic[letter][0] += 1
+//   })
+//   for (letter in dic){
+//       res.push([letter, dic[letter]])
+//   }
+//   let sortedRes = res.sort(function(a,b){
+//       return a[1][1] - b[1][1]
+//   })
+//   sortedRes.forEach(letter => {
+//       finalRes.push([letter[0],letter[1][0]])
+//   })
+//   return finalRes;
+// }
+
+// console.log(orderedCount("abracadabra"));
+// 8 kyu
+// Enumerable Magic - Does My List Include This?
+// DESCRIPTION:
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+function include(arr, item){
+    return arr.includes(item);
   }
-  let sortedRes = res.sort(function(a,b){
-      return a[1][1] - b[1][1]
-  })
-  sortedRes.forEach(letter => {
-      finalRes.push([letter[0],letter[1][0]])
-  })
-  return finalRes;
-}
-
-console.log(orderedCount("abracadabra"));
