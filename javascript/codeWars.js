88d428f0525493b580b7e2be28db999eb43bca53
Russs <russs.khalilov@gmail.com>
2024-01-04 12:20:54 +0300

    h_04.01..2024

--- javascript/codeWars.js
+++ javascript/codeWars.js
@@ -2703,6 +2703,40 @@
 //     How satisfied are you with this kata ? VERYSOMEWHATNONE
 // Grasshopper - Function syntax debugging
 // A student was working on a function and made some syntax mistakes while coding.Help them find their mistakes and fix them.
-function main(verb, noun) {
-    return verb + noun
-}
+// function main(verb, noun) {
+//     return verb + noun
+// }
+// 7 kyu
+// Number of People in the Bus
+// 159033291% of 10,33045,038 of 127,365aryan-firouzian
+// JavaScript
+// TRAIN AGAINNEXT KATA
+// Details
+// Solutions
+// Forks (28)
+// Discourse (223)
+// Translations
+// Fork|Collect|
+// DESCRIPTION:
+// There is a bus moving in the city which takes and drops some people at each bus stop.
+
+// You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+// Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+// The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+var number = function(busStops){
+    let res = 0
+    
+    for(stop of busStops){
+     res += stop[0] - stop[1]
+    }
+     return res
+   }
+
+   const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
+
